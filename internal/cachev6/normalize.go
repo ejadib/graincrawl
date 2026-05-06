e129ad2611f589408a7bc5e3080020bd110d02fa
Vincent Koc <vincentkoc@ieee.org>
2026-05-05 20:40:31 -0700

    feat(cache): normalize cache documents

---
+++ internal/cachev6/normalize.go
@@ -0,0 +1,65 @@
+package cachev6
+
+import (
+	"time"
+
+	"github.com/vincentkoc/graincrawl/internal/hashutil"
+	"github.com/vincentkoc/graincrawl/internal/model"
+	"github.com/vincentkoc/graincrawl/internal/timeutil"
+)
+
+func NoteFromDocument(doc Document, now time.Time) (model.Note, error) {
+	created, err := timeutil.Parse(doc.CreatedAt)
+	if err != nil {
+		return model.Note{}, err
+	}
+	updated, err := timeutil.Parse(doc.UpdatedAt)
+	if err != nil {
+		return model.Note{}, err
+	}
+	deleted, err := timeutil.ParsePtr(doc.DeletedAt)
+	if err != nil {
+		return model.Note{}, err
+	}
+	noteType := doc.Type
+	if noteType == "" {
+		noteType = "meeting"
+	}
+	return model.Note{
+		ID:            doc.ID,
+		Title:         doc.Title,
+		Type:          noteType,
+		Status:        doc.Status,
+		CreatedAt:     created,
+		UpdatedAt:     updated,
+		DeletedAt:     deleted,
+		WorkspaceID:   doc.WorkspaceID,
+		NotesPlain:    doc.NotesPlain,
+		NotesMarkdown: doc.NotesMarkdown,
+		Source:        model.SourceDesktopCache,
+		PayloadHash:   hashutil.JSON(doc),
+		LastSeenAt:    now,
+	}, nil
+}
+
+func TranscriptFromCache(chunk TranscriptChunk) (model.TranscriptChunk, error) {
+	start, err := timeutil.Parse(chunk.StartTimestamp)
+	if err != nil {
+		return model.TranscriptChunk{}, err
+	}
+	end, err := timeutil.Parse(chunk.EndTimestamp)
+	if err != nil {
+		return model.TranscriptChunk{}, err
+	}
+	return model.TranscriptChunk{
+		ID:                chunk.ID,
+		DocumentID:        chunk.DocumentID,
+		StartTimestamp:    start,
+		EndTimestamp:      end,
+		Source:            chunk.Source,
+		IsFinal:           chunk.IsFinal,
+		TranscriberUserID: chunk.TranscriberUserID,
+		Text:              chunk.Text,
+		PayloadHash:       hashutil.JSON(chunk),
+	}, nil
+}
